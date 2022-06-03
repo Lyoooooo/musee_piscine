c433fcd5c72a1b556c0f15581ef2438eb800b154
Lyoooooo <81254893+Lyoooooo@users.noreply.github.com>
2022-06-03 11:15:06 +0200

    FIN

--- expo.php
+++ expo.php
@@ -12,6 +12,7 @@
     <link rel="stylesheet" href="cssMusee.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css">
     <title>Document</title>
 </head>
 
@@ -37,12 +38,12 @@
                     <img src="images/logo.png" height=50>
                 </div>
                 <div class="col" id="second">
-                    MUSEE DU VIN ET DES SPIRITUEUX
+                    MUSEE DES VINS ET SPIRITUEUX
                 </div>
                 <div class="col" id="third">
                     <div class="dropdown">
                         <a class="btn sdropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
-                            <button class="btn btn-transparent" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight"><img src="avatar/<?php echo "$avatar" ?>" height="50"></button>
+                            <button class="btn btn-transparent" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight"><img src="avatar/<?php echo "$avatar" ?>" height="40"></button>
 
                             <div class="offcanvas offcanvas-end" style="background-color: #b22222;" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
                                 <div class="offcanvas-header">
@@ -179,9 +180,92 @@ nom musee
             </div>
         </div>
     </div>
-    <div class="foot">
-        info légales
-    </div>
+
+    <footer class='text-center text-white ' style='background-color: rgba(0, 0, 0, 0.904);color:white;'>
+  <!-- Grid container -->
+  <div class='container pt-4'>
+    <!-- Section: Social media -->
+    <section class='mb-4'>
+      <!-- Facebook -->
+      <a
+        class='btn btn-link btn-floating btn-lg text-light m-1'
+        href='#!'
+        role='button'
+        data-mdb-ripple-color='light'
+        ><i class='bi bi-facebook'></i
+      ></a>
+
+      <!-- Twitter -->
+      <a
+        class='btn btn-link btn-floating btn-lg text-light m-1'
+        href='#!'
+        role='button'
+        data-mdb-ripple-color='light'
+        ><i class='bi bi-twitter'></i
+      ></a>
+
+      <!-- Google -->
+      <a
+        class='btn btn-link btn-floating btn-lg text-light m-1'
+        href='#!'
+        role='button'
+        data-mdb-ripple-color='light'
+        ><i class='bi bi-google'></i
+      ></a>
+
+      <!-- Instagram -->
+      <a
+        class='btn btn-link btn-floating btn-lg text-light m-1'
+        href='#!'
+        role='button'
+        data-mdb-ripple-color='light'
+        ><i class='bi bi-instagram'></i
+      ></a>
+
+      <!-- Linkedin -->
+      <a
+        class='btn btn-link btn-floating btn-lg text-light m-1'
+        href='#!'
+        role='button'
+        data-mdb-ripple-color='light'
+        ><i class='bi bi-linkedin'></i
+      ></a>
+      <!-- Github -->
+      <a
+        class='btn btn-link btn-floating btn-lg text-light m-1'
+        href='#!'
+        role='button'
+        data-mdb-ripple-color='light'
+        ><i class='bi bi-github'></i
+      ></a>
+    </section>
+  
+  
+  
+    <!-- Section: Social media -->
+    <section class='text-center text-light'>
+  
+    <p>
+    Le Musée des Vins et Spiritueux est une marque deposée par ECE Bachelor. <br>
+    Tous droits réservés.
+    </p>
+
+        
+    
+    </section>
+  </div>
+  <!-- Grid container -->
+
+
+
+
+  <!-- Copyright -->
+  <div class='text-center text-light p-3' style='background-color: rgba(0, 0, 0, 0.2);'>
+    © 2022 M.V.S <br>Mentions légales :
+    <a href="https://www.flaticon.com/fr/icones-gratuites/avatar" title="avatar icônes">Avatar icônes créées par Prosymbols Premium - Flaticon</a>
+  </div>
+  <!-- Copyright -->
+</footer>
 </body>
 
 </html>
--- inscription.php
+++ inscription.php
@@ -144,7 +144,91 @@
     <div class="bg-image" style="background-image: url('images/back1.png');
             height: 50vh">
     </div>
+    <footer class='text-center text-white ' style='background-color: rgba(0, 0, 0, 0.904);color:white;'>
+  <!-- Grid container -->
+  <div class='container pt-4'>
+    <!-- Section: Social media -->
+    <section class='mb-4'>
+      <!-- Facebook -->
+      <a
+        class='btn btn-link btn-floating btn-lg text-light m-1'
+        href='#!'
+        role='button'
+        data-mdb-ripple-color='light'
+        ><i class='bi bi-facebook'></i
+      ></a>
 
+      <!-- Twitter -->
+      <a
+        class='btn btn-link btn-floating btn-lg text-light m-1'
+        href='#!'
+        role='button'
+        data-mdb-ripple-color='light'
+        ><i class='bi bi-twitter'></i
+      ></a>
+
+      <!-- Google -->
+      <a
+        class='btn btn-link btn-floating btn-lg text-light m-1'
+        href='#!'
+        role='button'
+        data-mdb-ripple-color='light'
+        ><i class='bi bi-google'></i
+      ></a>
+
+      <!-- Instagram -->
+      <a
+        class='btn btn-link btn-floating btn-lg text-light m-1'
+        href='#!'
+        role='button'
+        data-mdb-ripple-color='light'
+        ><i class='bi bi-instagram'></i
+      ></a>
+
+      <!-- Linkedin -->
+      <a
+        class='btn btn-link btn-floating btn-lg text-light m-1'
+        href='#!'
+        role='button'
+        data-mdb-ripple-color='light'
+        ><i class='bi bi-linkedin'></i
+      ></a>
+      <!-- Github -->
+      <a
+        class='btn btn-link btn-floating btn-lg text-light m-1'
+        href='#!'
+        role='button'
+        data-mdb-ripple-color='light'
+        ><i class='bi bi-github'></i
+      ></a>
+    </section>
+  
+  
+  
+    <!-- Section: Social media -->
+    <section class='text-center text-light'>
+  
+    <p>
+    Le Musée des Vins et Spiritueux est une marque deposée par ECE Bachelor. <br>
+    Tous droits réservés.
+    </p>
+
+        
+    
+    </section>
+  </div>
+  <!-- Grid container -->
+
+
+
+
+  <!-- Copyright -->
+  <div class='text-center text-light p-3' style='background-color: rgba(0, 0, 0, 0.2);'>
+    © 2022 M.V.S <br>Mentions légales :
+    <a href="https://www.flaticon.com/fr/icones-gratuites/avatar" title="avatar icônes">Avatar icônes créées par Prosymbols Premium - Flaticon</a>
+  </div>
+  <!-- Copyright -->
+</footer>
 </body>
 
 </html>
